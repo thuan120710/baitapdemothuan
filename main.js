914e85cf7d1545d88ca30a7cea2066cce74e941e
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-02-27 13:52:42 +0700

    Put and Delete

--- main.js
+++ main.js
@@ -28,10 +28,51 @@ app.get('/:id', (req, res) => {
     }
 })
 app.post('/', (req, res) => {
-    console.log(req.body);
+    //users.push(req.body);
+    let id = req.body.id;
+    if(typeof id == 'undefined'){
+        res.status(404).send({
+            message:"data khong lop le"
+        })
+    }else{
+        let checkExist = (users.filter(u=>u.id==id).length==0);
+        if(checkExist){
+            users.push(req.body);
+            res.status(200).send(users)
+        }else{
+            res.status(404).send({
+                message:"id khong lop le"
+            })
+        }
+    }
 })
 
-
+app.put('/:id',(req,res)=>{
+    let id = req.params.id;
+    let user = users.find(u=>u.id==id);
+    if(user){
+        for (const key of Object.keys(req.body)) {
+            user[key]=req.body[key];
+        }
+        res.status(200).send(user)
+    }else{
+        res.status(404).send({
+            message:"id khong lop le"
+        })
+    }
+})
+app.delete('/:id',(req,res)=>{
+    let id = req.params.id;
+    let user = users.find(u=>u.id==id);
+    if(user){
+        user.isDelete=true
+        res.status(200).send(user)
+    }else{
+        res.status(404).send({
+            message:"id khong lop le"
+        })
+    }
+})
 
 
 app.listen(port, () => {
